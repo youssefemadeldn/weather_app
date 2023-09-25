790af467f0afb2bbebdbf5ddb263b82ad6c08fac
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-25 23:21:24 +0300

    Handling exceptions to the request

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/widgets.dart';
 import 'package:weather_app/models/weather_model.dart';
 
 class WeatherService {
@@ -7,10 +8,14 @@ class WeatherService {
   final String apiKey = '8a4b821c8b174e0ab7940542232509';
 
   WeatherService({required this.dio});
-  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response =
-        await dio.get('$urlBase/forecast.json?key=$apiKey&q=$cityName&days=1');
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
+  Future<WeatherModel?> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio
+          .get('$urlBase/forecast.json?key=$apiKey&q=$cityName&days=1');
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on Exception catch (e) {
+      return null;
+    }
   }
 }
