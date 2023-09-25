9887b6fbb29e0fa0b1b74a61cce15dcf0189f866
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-25 14:47:56 +0300

    WeatherService and its method part 2

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -3,11 +3,19 @@ import 'package:weather_app/models/weather_model.dart';
 
 class WeatherService {
   final Dio dio;
+  final String urlBase = 'https://api.weatherapi.com/v1';
 
   WeatherService({required this.dio});
   Future<WeatherModel> getCurrentWeather({required String cityName}) async {
     Response response = await dio.get(
-        'https://api.weatherapi.com/v1/forecast.json?key=8a4b821c8b174e0ab7940542232509&q=$cityName&days=1');
+        '$urlBase/forecast.json?key=8a4b821c8b174e0ab7940542232509&q=$cityName&days=1');
+    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+    return weatherModel;
+  }
+
+  Future<WeatherModel> getForecastWeather({required String cityName}) async {
+    Response response = await dio.get(
+        '$urlBase/forecast.json?key=8a4b821c8b174e0ab7940542232509&q=$cityName&days=1');
     WeatherModel weatherModel = WeatherModel.fromJson(response.data);
     return weatherModel;
   }
