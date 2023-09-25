ded13bcee0a183ea5a9bbe03552b1e097237d491
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-25 09:27:32 +0300

    create WeatherModel part 2

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -15,4 +15,16 @@ class WeatherModel {
       required this.maxTemp,
       required this.minTemp,
       required this.weatherCondition});
+
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+        image: json['forecast']['forecastday'][0]['day']['condition']['icon'],
+        cityName: json['location']['name'],
+        date: json['current']['last_updated'],
+        temp: json['forecast']['forecastday'][0]['day']['avgtemp_c'],
+        maxTemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+        minTemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+        weatherCondition: json['forecast']['forecastday'][0]['day']['condition']
+            ['text']);
+  }
 }
