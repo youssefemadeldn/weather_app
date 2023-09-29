4aea65dd1a8e0fc9bbb2a33246a46dce8839fa80
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-09-29 18:01:36 +0300

    Create Function part 1

--- lib/cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -1,6 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class GetWeatherCubit extends Cubit<WeatherState> {
   GetWeatherCubit(super.initialState);
+
+  getWeather({required String cityName}) async {
+    WeatherModel weatherModel =
+        await WeatherService(Dio()).getCurrentWeather(cityName: cityName);
+  }
 }
