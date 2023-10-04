2b9c948783765e6297124313976215742175db39
youssef emad eldin <youssefemadeldn>
2023-10-04 22:47:12 +0200

    Create getThemeColor method

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+
 import 'package:weather_app/views/home_page.dart';
 
 void main() {
@@ -15,10 +16,75 @@ class WeatherApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => GetWeatherCubit(),
-      child: const MaterialApp(
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: getThemeColor(),
+        ),
         debugShowCheckedModeBanner: false,
-        home: MyHomePage(),
+        home: const MyHomePage(),
       ),
     );
+  }
+
+  MaterialColor getThemeColor(String weather) {
+    switch (weather) {
+      case "Sunny":
+        return Colors.orange;
+      case "Clear":
+        return Colors.lightBlue;
+      case "Partly cloudy":
+        return Colors.deepOrange;
+      case "Cloudy":
+        return Colors.grey;
+      case "Overcast":
+        return Colors.blueGrey;
+      case "Mist":
+        return Colors.indigo;
+      case "Patchy rain possible":
+      case "Patchy snow possible":
+      case "Patchy sleet possible":
+      case "Patchy freezing drizzle possible":
+      case "Blowing snow":
+      case "Blizzard":
+      case "Fog":
+      case "Freezing fog":
+      case "Patchy light drizzle":
+      case "Light drizzle":
+      case "Freezing drizzle":
+      case "Heavy freezing drizzle":
+      case "Patchy light rain":
+      case "Light rain":
+      case "Moderate rain at times":
+      case "Moderate rain":
+      case "Heavy rain at times":
+      case "Heavy rain":
+      case "Light freezing rain":
+      case "Moderate or heavy freezing rain":
+      case "Light sleet":
+      case "Moderate or heavy sleet":
+      case "Patchy light snow":
+      case "Light snow":
+      case "Patchy moderate snow":
+      case "Moderate snow":
+      case "Patchy heavy snow":
+      case "Heavy snow":
+      case "Ice pellets":
+      case "Light rain shower":
+      case "Moderate or heavy rain shower":
+      case "Torrential rain shower":
+      case "Light sleet showers":
+      case "Moderate or heavy sleet showers":
+      case "Light snow showers":
+      case "Moderate or heavy snow showers":
+      case "Light showers of ice pellets":
+      case "Moderate or heavy showers of ice pellets":
+      case "Patchy light rain with thunder":
+      case "Moderate or heavy rain with thunder":
+      case "Patchy light snow with thunder":
+      case "Moderate or heavy snow with thunder":
+        return Colors.grey;
+      default:
+        return Colors.blue;
+    }
   }
 }
