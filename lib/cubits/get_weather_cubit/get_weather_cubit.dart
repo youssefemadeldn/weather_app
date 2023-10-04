f3bcee73381d848560f2031d558af68a9b8e8d90
youssef emad eldin <youssefemadeldn>
2023-10-04 17:47:52 +0200

    Refactor on GetWeatherCubit

--- lib/cubits/get_weather_cubit/get_weather_cubit.dart
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -6,10 +6,10 @@ import 'package:weather_app/services/weather_service.dart';
 
 class GetWeatherCubit extends Cubit<WeatherState> {
   GetWeatherCubit() : super(WeatherInitialState());
-
+  late WeatherModel weatherModel;
   getWeather({required String cityName}) async {
     try {
-      WeatherModel weatherModel =
+      weatherModel =
           await WeatherService(Dio()).getCurrentWeather(cityName: cityName);
       emit(WeatherLodedState());
     } catch (e) {
