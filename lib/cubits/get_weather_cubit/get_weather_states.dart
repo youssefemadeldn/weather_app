641ad83b7680a622a865413eb8410f85255e6829
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-10-05 11:39:53 +0200

    Receive WeatherModel in my state part 1

--- lib/cubits/get_weather_cubit/get_weather_states.dart
+++ lib/cubits/get_weather_cubit/get_weather_states.dart
@@ -1,7 +1,12 @@
+import 'package:weather_app/models/weather_model.dart';
+
 class WeatherState {}
 
 class WeatherInitialState extends WeatherState {}
 
-class WeatherLodedState extends WeatherState {}
+class WeatherLodedState extends WeatherState {
+  WeatherLodedState(this.weatherModel);
+  final WeatherModel weatherModel;
+}
 
 class WeatherFailurState extends WeatherState {}
