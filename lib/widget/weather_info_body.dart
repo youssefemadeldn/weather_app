34f16c3b2c9b5e023b594b7f6544f00eebb8d6b9
youssef emad eldin <youssefemad63.ye@gmail.com>
2023-10-05 08:08:17 +0200

    solve Exception on WeatherInfoBody part 3

--- lib/widget/weather_info_body.dart
+++ lib/widget/weather_info_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/models/weather_model.dart';
 
 class WeatherInfoBody extends StatelessWidget {
   const WeatherInfoBody({
@@ -9,7 +10,8 @@ class WeatherInfoBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var weatherModel = BlocProvider.of<GetWeatherCubit>(context).weatherModel;
+    WeatherModel weatherModel =
+        BlocProvider.of<GetWeatherCubit>(context).weatherModel!;
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
